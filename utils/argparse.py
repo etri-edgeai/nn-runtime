a3e03c81a8ed36c40b17404e7fa413bf8b402d9d
Sanggeon Park <gunnih@gmail.com>
2022-08-02 15:49:29 +0900

    define ThrowingArgumentParser class

--- utils/argparse.py
+++ utils/argparse.py
@@ -1,3 +1,7 @@
 import argparse
 
 class ArgumentParserError(Exception): pass
+
+class ThrowingArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise ArgumentParserError(message)
