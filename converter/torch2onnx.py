643c334c78e9e7ca55f006593d77942103219325
Sanggeon Park <gunnih@gmail.com>
2022-08-04 14:22:04 +0900

    added convert_torch2onnx function

--- converter/torch2onnx.py
+++ converter/torch2onnx.py
@@ -1,2 +1,28 @@
 from typing import Tuple
-from utils.argparse import ThrowingArgumentParser
+
+def convert_torch2onnx(
+    torch_model_path,
+    output_onnx_model_path,
+    input_shape: Tuple,
+    input_names=['input'],
+    output_names=['output'],
+    opset_version=12):
+
+    try:
+        import torch
+        torch_model = torch.load(torch_model_path)
+        torch_model.eval()
+        dummy_input = torch.rand(input_shape)
+        torch.onnx.export(
+            torch_model,
+            dummy_input,
+            output_onnx_model_path,
+            verbose=True,
+            input_names=input_names,
+            output_names=output_names,
+            opset_version=opset_version)
+    except Exception as e:
+        print(f'export to onnx error: {str(e)}')
+        return False, e
+
+    return True, None
