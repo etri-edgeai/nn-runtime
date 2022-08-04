bbed6c4fa66df92337251558d663ebca25de7b81
Sanggeon Park <gunnih@gmail.com>
2022-08-04 14:23:39 +0900

    added torch2onnx_argument_parser function

--- utils/argparse.py
+++ utils/argparse.py
@@ -5,3 +5,14 @@ class ArgumentParserError(Exception): pass
 class ThrowingArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         raise ArgumentParserError(message)
+
+def torch2onnx_argument_parser() -> ThrowingArgumentParser:
+    parser = ThrowingArgumentParser()
+    parser.add_argument('--torch',type=str, required=True, help='input torch model path (ex: /path/to/torch/file.pt')
+    parser.add_argument('--onnx',type=str, required=True, help='output onnx model path (ex: /path/to/file.onnx')
+    parser.add_argument('--input-shape', nargs='+', type=int, required=True, help='model input shape (ex: 1 3 128 128)')
+    parser.add_argument('--input-names', nargs='+', default=['input'], help='input layer names (ex: input1 input2 input3)')
+    parser.add_argument('--output-names', nargs='+', default=['output'], help='output layer names (ex: output1 output2 output3)')
+    parser.add_argument('--opset-version',type=int, default=12, help='onnx operation set version (ex: 12')
+    return parser
+
