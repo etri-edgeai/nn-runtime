caeb5d7a23215288eb3dc0a8cdb45bff6a013166
Sanggeon Park <gunnih@gmail.com>
2022-08-16 09:32:19 +0900

    removed openvino dependency and using onnx-tf

--- converter/onnx2tflite.py
+++ converter/onnx2tflite.py
@@ -1,5 +1,4 @@
-from enum import Enum
-import os, sys, pathlib, subprocess, shutil
+import os, subprocess, shutil
 from utils.general import file_size
 from utils.argparse import ModelDataType
 
@@ -7,83 +6,49 @@ _default_tensorflow_output_path = './temp/tensorflow_output'
 _default_tflite_output_path = './temp/tflite_output'
 _default_saved_pb_file_name = 'saved_model.pb'
 
-def export_onnx2openvino(onnx_path,
-                        output_dir=_default_openvino_output_path,
-                        dtype=ModelDataType.FP32):
+def export_onnx2tensorflow(onnx_path, output_dir=_default_tensorflow_output_path):
     """
-       Converting onnx to openvino
-       Returns openvino output path and an exception if an exception is raised.
+       Converting onnx to tensorflow
+       Returns exported tensorflow output path and an exception if an exception is raised.
     """
     try:
-        #check_requirements(('openvino-dev',))  # requires openvino-dev: https://pypi.org/project/openvino-dev/
-        import openvino.inference_engine as ie
-
-        print(f'{__file__} starting export with openvino {ie.__version__}...')
-
-        #os.makedirs('../opt/output', exist_ok=True)
         os.makedirs(output_dir, exist_ok=True)
-        cmd = f"mo --input_model {onnx_path} --output_dir {output_dir} --data_type {dtype.name}"
+        cmd = f"onnx-tf convert -i {onnx_path} --outdir {output_dir}"
         subprocess.check_output(cmd.split())  # export
-
         print(f'{__file__} export success, saved as {output_dir} ({file_size(output_dir):.1f} MB)')
-        file_list = os.listdir(output_dir)
-        for ff in file_list:
-            if pathlib.Path(ff).suffix == ".xml":
-                tmp_path = os.path.join(output_dir, ff)
-                os.rename(tmp_path, os.path.join(output_dir, _default_openvino_xml_file_name))
-            elif pathlib.Path(ff).suffix == ".bin":
-                tmp_path = os.path.join(output_dir, ff)
-                os.rename(tmp_path, os.path.join(output_dir, _default_openvino_bin_file_name))
-            else: # mapping
-                tmp_path = os.path.join(output_dir, ff)
-                os.rename(tmp_path, os.path.join(output_dir, _default_openvino_map_file_name))
         return output_dir, None
     except Exception as e:
         print(f'export failure: {e}')
         return output_dir, e
-        
 
-def export_openvino2tensorflow(openvino_path=_default_openvino_output_path,
-                                openvino_xml_file_name=_default_openvino_xml_file_name,
-                                tensorflow_file_path=_default_tensorflow_file_path):
-
-    try:
-        openvino_xml_file = os.path.join(openvino_path, openvino_xml_file_name)
-        cmd = f"openvino2tensorflow --model_path {openvino_xml_file} --model_output_path {tensorflow_file_path} --output_saved_model"
-        subprocess.check_output(cmd.split())  # export
-        print(f'{__file__} export success, saved as {tensorflow_file_path} ({file_size(tensorflow_file_path):.1f} MB)')
-        return tensorflow_file_path, None
-    except Exception as e:
-        print(f'export failure: {e}')
-        return tensorflow_file_path, e
-
-
-def export_tensorflow2tflite(dtype:str, output_dir, tensorflow_file_path=_default_tensorflow_file_path):
+def export_tensorflow2tflite(dtype=ModelDataType.FP32,
+                            tensorflow_path=_default_tensorflow_output_path,
+                            saved_pb_file_name=_default_saved_pb_file_name,
+                            output_dir=_default_tflite_output_path):
     # TF-Lite export
+    os.makedirs(output_dir, exist_ok=True)
     try:
         print(f'{__file__} starting export with tflite...')
-
-        output_dir = '../opt/output/'
-        weights = "../opt/output/output_saved_model"
-        if dtype == "FP32":
-            tflite_model = convert_tflite_fp32(weights)    
+        if dtype.name == "FP32":
+            tflite_model = convert_tflite_fp32(tensorflow_path)    
             file_name = "output_fp32.tflite"
-        elif dtype == "FP16":
-            tflite_model = convert_tflite_fp32(weights)
+        elif dtype.name == "FP16":
+            tflite_model = convert_tflite_fp32(tensorflow_path)
             file_name = "output_fp16.tflite"
         else:
-            tflite_model = convert_tflite_int8(weights)
+            tflite_model = convert_tflite_int8(tensorflow_path)
             file_name = "output_int8.tflite"
 
         output_model_path = os.path.join(output_dir, file_name)
         with open(output_model_path, "wb") as f:
             f.write(tflite_model)                
-        # remove openvino & tensorflow model 
-        shutil.rmtree("../opt/output/output_openvino")#, ignore_errors=False, onerror=None)
-        shutil.rmtree("../opt/output/output_saved_model")#, ignore_errors=False, onerror=None)
+        
+        # remove tensorflow model 
+        #shutil.rmtree(tensorflow_path)
 
         print(f'{__file__} export success, saved as {output_model_path} ({file_size(output_model_path):.1f} MB)')
         return output_model_path
+
     except Exception as e:
         print(f'export failure: {e}')
 
