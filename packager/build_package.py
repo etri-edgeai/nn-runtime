344aef0920946068899ec06c0d590266ff7856bb
Sanggeon Park <gunnih@gmail.com>
2022-09-21 23:51:37 +0900

    implemented build_package

--- packager/build_package.py
+++ packager/build_package.py
@@ -100,3 +100,169 @@ def copy_model(model_path, package_name, framework, distribution) -> str:
     shutil.copyfile(model_path, dest_model_file_path)
     return dest_model_file_path
 
+
+def _build(framework, platform, pkg_version, pkg_py_version, pkg_name, distribution, is_obf):
+    reqs_list = ["opencv-python"]
+    if framework == "tflite":
+        if platform == "linux.x86_64":
+            reqs_list.append("tensorflow")
+        elif platform == "linux.armv7":
+            if pkg_py_version == "py37":
+                reqs_list.append(
+                    f"tflite_runtime @ https://github.com/nota-github/tfliteruntime_bin/releases/download/v2.9.0/tflite_runtime-2.9.0rc1-cp{pkg_py_version.split('py')[-1]}-cp{pkg_py_version.split('py')[-1]}m-linux_armv7l.whl"
+                )
+            else:
+                reqs_list.append(
+                    f"tflite_runtime @ https://github.com/nota-github/tfliteruntime_bin/releases/download/v2.9.0/tflite_runtime-2.9.0rc1-cp{pkg_py_version.split('py')[-1]}-cp{pkg_py_version.split('py')[-1]}-linux_armv7l.whl"
+                )
+        elif platform == "linux.aarch64":
+            reqs_list.append(
+                f"tflite_runtime @ https://github.com/nota-github/tfliteruntime_bin/releases/download/v2.9.0/tflite_runtime-2.9.0rc0-cp{pkg_py_version.split('py')[-1]}-none-linux_aarch64.whl"
+            )
+    
+    packages = [
+        f"{pkg_name}",
+        f"{pkg_name}.libs",
+        f"{pkg_name}.libs.{framework}",
+        f"{pkg_name}.models",
+        f"{pkg_name}.utils",
+    ]
+    package_data = {
+        f"{pkg_name}.libs.{framework}" : ['*.tflite', '*.trt', '*.engine'],
+    }
+    platform_dict = {
+        "linux.x86_64":"linux.x86_64",
+        "linux.armv7":"linux.armv7l",
+        "linux.aarch64":"linux.aarch64"
+    }
+
+    setup(
+        name=pkg_name,
+        version=pkg_version,
+        description="",
+        long_description="",
+        platforms="Posix;",
+        zip_safe=False,
+        python_requires="",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Operating System :: POSIX :: Linux",
+            f"Programming Language :: Python :: {pkg_py_version}"
+        ],
+        include_package_data=True,
+        packages=packages,
+        package_dir={
+            "": os.path.relpath(os.path.join(distribution), os.getcwd())
+        },
+        package_data=package_data,
+        install_requires=reqs_list,
+        script_args = [
+            '-q',
+            'build', 
+            '--build-purelib', os.path.join(distribution,"edge_ai"),
+            
+            "bdist_wheel",
+            "--plat-name", platform_dict[platform],
+            "--python-tag", pkg_py_version,
+            "--dist-dir", distribution,
+            "--bdist-dir", os.path.join(distribution,"edge_ai"),
+        ]
+    )
+
+def build_package(data:dict):
+    model_url:str = data.get("model_url")
+    source_path:str = data.get("source_path")
+    model_file:bytes = data.get("model_file")
+    package_name:str = data.get("package_name")
+    platform:str = data.get("platform")
+    framework:str = data.get("framework")
+    package_version:str = data.get("package_version")
+    is_obf:bool = data.get("obf")
+    python_version:str = data.get("python_version")
+
+    # mkdir -> copy -> template -> (obf) -> build & package
+    task_id = str(uuid.uuid4())
+    logging.info(f'Build start taskID: {task_id}')
+    _distribute = os.path.abspath(os.path.join("/tmp/work", ''.join(random.choice(string.ascii_lowercase + '0123456789') for i in range(8))))
+    _base = os.path.dirname(__file__)
+    logging.info(f'Create package output folder {_distribute}')
+    if os.path.exists(_distribute):
+        try:
+            shutil.rmtree(_distribute)
+        except:
+            logging.error(f'{_distribute} remove failed')
+    os.makedirs(_distribute, exist_ok=False)
+    shutil.copytree(os.path.join(_base, "templates"), os.path.join(_distribute, package_name))
+
+    logging.info(f'prepare model for packaging')
+    model_path:str = copy_model(
+        model_url=model_url,
+        model_file=model_file,
+        package_name=package_name,
+        framework=framework,
+        distribution=_distribute
+    )
+
+    template_model_name={
+        "tflite":"tflite",
+        "trt":"engine"
+    }
+    inference_template_var = {
+        template_model_name[framework]:model_path.split("/")[-1],
+        "is_obf":is_obf
+    }
+
+    # template
+    logging.info(f'Make template')
+    env = Environment(loader=FileSystemLoader(os.path.join(_distribute, package_name)))
+    make_template(
+        env, f"libs/{framework}/inference.py",
+        os.path.join(_distribute, package_name),
+        inference_template_var
+    )
+    if source is not None:
+        with open(os.path.join(_distribute, package_name, "models/model.py"), "w") as f:
+            f.write(source)
+    else:
+        os.remove(os.path.join(_distribute, package_name, "models/model.py"))
+    source = ""
+    if source_path is not None:
+        source_file = open(source_path, "r")
+        source = source_file.read()
+
+    make_template(
+        env, "models/model.py", 
+        os.path.join(_distribute, package_name),
+        {"source":source or ""}
+    )
+    make_template(
+        env, "models/base.py",
+        os.path.join(_distribute, package_name),
+        {"framework":framework}
+    )
+    
+    # obf
+    if is_obf:
+        logging.info(f'Obfuscate package')
+        #_distribute = obfuscate(platform, python_version, _distribute, package_name, framework, model_files)
+
+    logging.info(f'Build package')
+    _build(
+        framework=framework, 
+        platform=platform, 
+        pkg_py_version=python_version, 
+        pkg_version=package_version, 
+        pkg_name=package_name, 
+        distribution=_distribute, 
+        is_obf=is_obf
+    )
+    
+    # upload
+    whl_file = [i for i in os.listdir(_distribute) if os.path.splitext(i)[1] == '.whl'][0]
+    logging.info(f'Upload `whl` file {whl_file}')
+    
+    logging.info(f'Remove {_distribute}')
+    if is_obf:
+        _distribute = os.path.dirname(_distribute)
+    shutil.rmtree(_distribute)
+    return
